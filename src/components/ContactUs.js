1d59bd7b4cbc77bc31528fdfbb18d64315ed4654
Abdulbasit335 <113461803+basir2353@users.noreply.github.com>
2023-12-25 10:44:07 +0500

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,7 +7,7 @@ const ContactSection = () => {
   const [copyMessageVisible, setCopyMessageVisible] = useState(false);
 
   const copyPhoneNumber = () => {
-    const phoneNumber = '+91 8980500565';
+    const phoneNumber = '+923469517653';
     navigator.clipboard.writeText(phoneNumber)
       .then(() => {
         setCopyMessageVisible(true);
@@ -33,7 +33,7 @@ const ContactSection = () => {
           </div>
         </div>
         <div className="contact-options">
-          <h1>Email: <a href="mailto:reachsagarshah@gmail.com">abasit5612345@gmail.com</a></h1>
+          <h1>Email: <a href="mailto:mabdulbasitdogar@gmail.com"> mabdulbasitdogar@gmail.com</a></h1>
           <h1><RiPhoneLine size={18} />Phone: +923469517653 <span className="copy-icon" onClick={copyPhoneNumber}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard2-check" viewBox="0 0 16 16">
             <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z"/>
             <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z"/>
